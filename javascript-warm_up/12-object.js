6c1367d5e957ae4503a0f0b0c2cf2c0182911398
toye1990 <ifetoyeobafemi@gmail.com>
2023-09-19 21:29:06 +0100

    sent

---
+++ javascript-warm_up/12-object.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// create an object
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+
+//replace the value 12 with 89
+myObject.value = 89;
---
+++ javascript-warm_up/13-add.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// create a function add
+function add(a, b){
+  return a + b;
+}
+add(3, 5)
