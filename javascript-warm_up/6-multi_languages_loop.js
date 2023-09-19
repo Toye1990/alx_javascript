c04382824789862457ad4a9107c8f3e128682551
toye1990 <ifetoyeobafemi@gmail.com>
2023-09-19 02:19:58 +0100

    used for loop

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+//create a const array variable
+const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+//iterate through array using for loop
+for(let i=0; i < array.length; i++){
+    console.log(array[i]);
+}
