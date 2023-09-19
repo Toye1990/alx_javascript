d67306f582d73390967829b70a346f0d44c6c50d
toye1990 <ifetoyeobafemi@gmail.com>
2023-09-19 02:01:26 +0100

    java 1 done

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,10 @@
+//create a variable
+const arrays = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+//iterate through array to print strings
+for (const array of arrays) {
+    console.log(array);
+  }
+
+
+
