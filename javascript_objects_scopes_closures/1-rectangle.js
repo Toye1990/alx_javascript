1a8a9b32536fdc243d019b628023f3f612cbe8ae
toye1990 <ifetoyeobafemi@gmail.com>
2023-09-23 14:52:28 +0100

    do

--- javascript_objects_scopes_closures/1-rectangle.js
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -1,9 +1,13 @@
-//create a new class
-
-class Rectangle{
-    constructor(w, h){
-     this.width = w;
-     this.height = h;
+#!/usr/bin/node
+class Rectangle {
+    constructor(w, h) {
+      this.width = w;
+      this.height = h;
     }
-}
+  }
+  
+  const r1 = new Rectangle(2, 3);
+  console.log(r1);
+  console.log(r1.width);
+  console.log(r1.height);
 
