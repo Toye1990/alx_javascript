6be868b2cd07db645deac77dcc62f9a9326263b6
toye1990 <ifetoyeobafemi@gmail.com>
2023-09-29 01:46:38 +0100

    try this

--- javascript-network/0-statuscode.js
+++ javascript-network/0-statuscode.js
@@ -1,20 +1,20 @@
 #!/usr/bin/node
 
-const req = require('request');
+/*const req = require('request');
 
 async function displayNow(url){
   const awat = await req.get(url);
   const status = awat.statusCode;
   console.log(`code: ${status}`);
-}
+}*/
 
 
 
-/*const req = require('request');
+const req = require('request');
 
 function displayUrl(url){
     req.request(url)
     .on('response', response => {
         console.log('code: ', response.statusCode);
     })
-}*/
+}
