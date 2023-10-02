1a6e7d6860b26d37399df12a964df87ce7383e90
toye1990 <ifetoyeobafemi@gmail.com>
2023-10-02 14:12:24 +0100

    try now

--- javascript-network/0-statuscode.js
+++ javascript-network/0-statuscode.js
@@ -1,15 +1,27 @@
 #!/usr/bin/node
 
-const request = require('request');
+//const request = require('request');
 
 
-function statUrl(url){
+/*function statUrl(url){
   request.get(url, (error, response) => {
   if (error){
     console.error('error:', error)
   }else{
     console.log(`code: ${response.statusCode}`);
   }
+  });
+}*/
+
+const request = require('request');
+
+function displayStatusCode(url) {
+  request.get(url, (error, response) => {
+    if (error) {
+      console.error('Error:', error);
+    } else {
+      console.log(`Code: ${response.statusCode}`);
+    }
   });
 }
 
