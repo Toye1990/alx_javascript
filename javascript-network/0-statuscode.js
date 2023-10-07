57c5aba1d43c40e9fc72bed6e777b126a08477ca
toye1990 <ifetoyeobafemi@gmail.com>
2023-10-07 14:52:04 +0100

    try1

--- javascript-network/0-statuscode.js
+++ javascript-network/0-statuscode.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-
+const process = require('process');
 const request = require('request');
-
+const url = process.argv[2];
 
 function responUrl(url){
   request.get(url, (error, response) => {
@@ -12,3 +12,5 @@ function responUrl(url){
   }
   });
 }
+
+responUrl();
